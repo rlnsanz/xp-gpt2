0d13dda0aaced4868ef92c2fd63b3448ef7bfb66
Rolando Garcia <rogarcia@berkeley.edu>
2023-04-24 19:58:56 +0000

    Squashed commit of the following:
    commit be7af9c8881d8947ce672d2256e54d6920fabb20
    Author: Rolando Garcia <rogarcia@berkeley.edu>
    Date:   Mon Apr 24 19:57:13 2023 +0000
    
        RECORD::debugRun
    
    commit d378829385ace0c81f4ddc87d35fd85f1cb4d397
    Author: Rolando Garcia <rogarcia@berkeley.edu>
    Date:   Mon Apr 24 19:47:38 2023 +0000
    
        RECORD::debugRun
    
    commit cb1d6639dba59ba193c0f7730a2cc7da674e3598
    Author: Rolando Garcia <rogarcia@berkeley.edu>
    Date:   Mon Apr 24 19:45:55 2023 +0000
    
        RECORD::debugRun
    
    commit b08bc1dc366565213b5fc1a80d79b08e401a7622
    Author: Rolando Garcia <rogarcia@berkeley.edu>
    Date:   Mon Apr 24 19:45:16 2023 +0000
    
        RECORD::debugRun
    
    commit a34d7c3d0fc41542d27d1030b2967f03189116b9
    Author: Rolando Garcia <rogarcia@berkeley.edu>
    Date:   Mon Apr 24 19:43:36 2023 +0000
    
        RECORD::debugRun
    
    commit 7fe97e7e846bcd9c71490bf2fc9b60ed2633af72
    Author: Rolando Garcia <rogarcia@berkeley.edu>
    Date:   Mon Apr 24 19:42:15 2023 +0000
    
        RECORD::debugRun
    
    commit 1956931dc4fa387b312e52b99f3ad80a29d3321c
    Author: Rolando Garcia <rogarcia@berkeley.edu>
    Date:   Mon Apr 24 19:25:44 2023 +0000
    
        RECORD::debugRun

--- train.py
+++ train.py
@@ -16,7 +16,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Hyper-parameters
 num_epochs = 5
 learning_rate = 0.001
-max_length = 32
+max_length = 64
+batch_size = 4
 
 # Data loader
 data = load_dataset("wikipedia", "20220301.en")
@@ -28,13 +29,13 @@ assert isinstance(data["train"], Dataset)
 assert set(data["train"].features) == {"id", "url", "title", "text"}
 
 model_name = "gpt2-large"
-feature_extractor = AutoTokenizer.from_pretrained("cerebras/Cerebras-GPT-111M")
+feature_extractor = GPT2Tokenizer.from_pretrained(model_name)
 model = GPT2Model.from_pretrained(model_name).to(device)  # type: ignore
 Flor.checkpoints(model)
-feature_extractor.padding_side = "right"
-feature_extractor.pad_token_id = feature_extractor.eos_token_id
-feature_extractor.pad_token = feature_extractor.eos_token
-# feature_extractor.add_tokens({"pad_token": feature_extractor.eos_token, "pad_token_id": feature_extractor.eos_token_id})  # type: ignore
+# feature_extractor.padding_side = "right"
+# feature_extractor.pad_token_id = feature_extractor.eos_token_id
+# feature_extractor.pad_token = feature_extractor.eos_token
+feature_extractor.add_special_tokens({"pad_token": "[PAD]"})  # type: ignore
 
 
 def my_collate(batch):
@@ -46,6 +47,8 @@ def my_collate(batch):
     original_text = batch[0]["text"]
     new_features = []
     for i, sentence in enumerate(original_text.split("\n")):
+        if not sentence:
+            continue
         featurized = feature_extractor(
             sentence,
             return_tensors="pt",
@@ -55,13 +58,16 @@ def my_collate(batch):
         )
         new_features.append(featurized)
 
-    paired_features = [
-        (new_features[i], new_features[(i + 1) % len(new_features)])
-        for i in range(len(new_features))
-    ]
-    paired_features = torchdata.default_collate(paired_features)
+    while new_features:
+        chunk_features = new_features[0:batch_size]
+        new_features = new_features[batch_size:]
 
-    return paired_features
+        paired_features = [
+            (chunk_features[i], chunk_features[(i + 1) % batch_size])
+            for i in range(batch_size)
+        ]
+        paired_features = torchdata.default_collate(paired_features)
+        yield paired_features
 
 
 train_loader = torchdata.DataLoader(dataset=data["train"].with_format("torch"), batch_size=1, shuffle=False, collate_fn=my_collate)  # type: ignore
@@ -76,35 +82,31 @@ total_step = len(train_loader)
 num_steps = 1000
 for epoch in Flor.loop(range(num_epochs)):
     model.train()
-    for i, (batch, target) in Flor.loop(enumerate(train_loader)):
-        # Move tensors to the configured device
-        # text = feature_extractor.decode(each) for each in batch["input_ids"]
-        batch = batch.to(device)
-        target = target.to(device)
+    for i, wiki_gen in Flor.loop(enumerate(train_loader)):
+        for (batch, target) in wiki_gen:
+            # Move tensors to the configured device
+            # text = feature_extractor.decode(each) for each in batch["input_ids"]
+            batch = batch.to(device)
+            target = target.to(device)
 
-        # Forward pass
-        outputs = model(**batch)
-        print("hold")
-        # loss = criterion(
-        #     outputs.last_hidden_state.reshape(batch_size, -1, max_length),
-        #     batch["input_ids"],
-        # )
-
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if i % 100 == 0:
-            print(
-                "Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}".format(
-                    epoch + 1, num_epochs, i, num_steps, flor.log("loss", loss.item())
-                )
+            # Forward pass
+            outputs = model(**batch)
+            print("hold")
+            loss = criterion(
+                outputs.last_hidden_state.reshape(batch_size, -1, max_length),
+                target["input_ids"].reshape(batch_size, max_length),
             )
-            if i == num_steps:
-                # bootleg sampling
-                break
 
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(
+            "Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}".format(
+                epoch + 1, num_epochs, i, num_steps, flor.log("loss", loss.item())  # type: ignore
+            )
+        )
     print("Model Validate")
 
 # Test the model
